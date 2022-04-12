64d7cc7efc69a7429e039c7c555336e88e5b2ed4
aula <martinconcetti97@gmail.com>
2022-04-12 12:41:56 -0300

    Disable cube rigid body

---
+++ Towe bloxx/Assets/joints.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class joints : MonoBehaviour
+{
+    private Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        rb.isKinematic = true;
+        Debug.Log("fuck");
+    }
+}
